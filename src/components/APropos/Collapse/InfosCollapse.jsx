4d1be56286622ad1536407c7685b1570e9773a6d
Théo Brasseur <theobrasseurlille@hotmail.com>
2023-12-20 00:59:17 +0100

    icon collapse + transition

--- src/components/APropos/Collapse/InfosCollapse.jsx
+++ src/components/APropos/Collapse/InfosCollapse.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon  } from '@fortawesome/react-fontawesome'; // npm i @fortawesome/react-fontawesome
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'; // npm i @fortawesome/free-solid-svg-icons
 
 function InfosCollapse({ title, content }) {
     const [isOpen, setIsOpen] = useState(false);
@@ -11,6 +13,7 @@ function InfosCollapse({ title, content }) {
           <div className='collapse_button' onClick={toggleCollapse}>
             <h2>{title}</h2>
             <div className={`arrow-container ${isOpen ? "arrow-container-up" : ""}`}>
+            <FontAwesomeIcon icon={faAngleDown} className='arrow' />
             </div>
           </div>
           {isOpen && <div className='collapse_content'>{content}</div>}
